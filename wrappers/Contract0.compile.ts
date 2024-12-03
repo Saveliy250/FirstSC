34ffe42b6537c44de72c68a606433a2ab23dcb36
Saveliy250 <savepo@icloud.com>
2024-12-03 03:19:02 +0300

    simple counter

--- wrappers/Contract0.compile.ts
+++ wrappers/Contract0.compile.ts
@@ -2,5 +2,5 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'func',
-    targets: ['contracts/contract0.fc'],
+    targets: ['contracts/counter.fc'],
 };
